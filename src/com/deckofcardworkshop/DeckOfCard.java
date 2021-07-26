8b74906c0a9d0f2118ec95b152d67953723d853a
SamarthBM <samarthbm18@gmail.com>
2021-07-26 09:10:05 +0530

    Initial commit

---
+++ src/com/deckofcardworkshop/DeckOfCard.java
@@ -0,0 +1,16 @@
+/****************************************************************
+ * Purpose : To perform Deck of card game.
+ * @author Samarth BM
+
+***************************************************************/
+
+package com.deckofcardworkshop;
+
+public class DeckOfCard {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Deck Of Card game");
+
+	}
+
+}
